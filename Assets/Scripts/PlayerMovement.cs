6a815e8d8991cc22563307b4ed9890241d9bebe2
bubuza <almatbotabekov@gmail.com>
2022-04-07 18:03:59 +0600

    create unity project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float speed = 10f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3f;
+    public float groundDistance = 0.1f;
+    private Vector3 velocity;
+
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+       
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+//jump 
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+//sit
+        if (Input.GetKey("left ctrl"))
+        {
+            controller.height = 1f;
+        }
+        else
+        {
+            controller.height = 2f;
+        }
+//speedForce
+        if (Input.GetKey("left shift"))
+        {
+            speed = 20f;
+        }
+        else
+        {
+            speed = 10f;
+        }
+    }
+    
+}
